942404a21480329d6b09e571b8a509868090ce9e
wuyeelok <wuyeelok@gmail.com>
2025-02-20 09:10:09 +0800

    Begin part 15

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Part 15");
+
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+Console.Write("List: ");
+foreach (var number in numbers)
+{
+    if (numbers.IndexOf(number) == numbers.Count - 1)
+    { Console.Write(number); }
+    else
+    { Console.Write($"{number} "); }
+}
+Console.WriteLine();
+Console.WriteLine($"Check index of 4 in the list: {numbers.IndexOf(4)}");
